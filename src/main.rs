d613e8ea37d553b68b913df1ce55bde29bd2838f
Alexander Letourneau <alex@Alexanders-MacBook-Pro.local>
2021-11-22 22:00:56 -0500

    Add first attempt at functional arg parser

--- src/main.rs
+++ src/main.rs
@@ -2,66 +2,64 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::str::FromStr;
 use std::time::Instant;
+use std::process::exit;
 
 fn main() {
-    let sub_command = std::env::args().nth(1).expect("No sub command given");
-    let sub_sub_command = std::env::args().nth(2).expect("No sub sub command given");
+    let sub_command = std::env::args().nth(1);
 
-    match SubCommand::from_str(&sub_command) {
-        Ok(SubCommand::Vehicle) => {
-            match VehicleSubCommand::from_str(&sub_sub_command) {
-                Ok(VehicleSubCommand::Add) => { println!("Add vehicle"); }
-                Ok(VehicleSubCommand::Remove) => { println!("Remove vehicle"); }
-                Ok(VehicleSubCommand::List) => { println!("List vehicles"); }
-                Ok(VehicleSubCommand::Get) => { println!("Get vehicle"); }
-                Ok(VehicleSubCommand::Update) => { println!("Update vehicle"); }
-                Err(()) => { println!("Invalid vehicle subcommand \"{}\"", sub_sub_command)}
+    if sub_command.is_none() {
+        println!("No command passed, exiting...");
+        exit(1);
+    }
+
+    let sub_command = sub_command.unwrap();
+
+    let sub_command = match SubCommand::from_str(&sub_command) {
+        Ok(i) => { i }
+        Err(()) => { 
+            println!("Invalid subcommand \"{}\", exiting...", sub_command);
+            exit(1);
+        }
+    };
+
+    match sub_command {
+        SubCommand::Vehicle => {
+            let vehicle_command = std::env::args().nth(2);
+
+            if vehicle_command.is_none() {
+                println!("No command passed, exiting...");
+                exit(1);
+            }
+        
+            let vehicle_command = vehicle_command.unwrap();
+        
+            let vehicle_command = match VehicleSubCommand::from_str(&vehicle_command) {
+                Ok(i) => { i }
+                Err(()) => { 
+                    println!("Invalid subcommand \"{}\", exiting...", vehicle_command);
+                    exit(1);
+                }
+            };
+
+            match vehicle_command {
+                VehicleSubCommand::Add => { println!("Add vehicle"); }
+                VehicleSubCommand::Remove => { println!("Remove vehicle"); }
+                VehicleSubCommand::List => { println!("List vehicles"); }
+                VehicleSubCommand::Get => {
+                    let vehicle_name = std::env::args().nth(3);
+
+                    if vehicle_name.is_none() {
+                        println!("No vehicle name specified, exiting...");
+                        exit(1);
+                    }
+
+                    let cars = load_cars();
+                    dbg!(cars.get(&vehicle_name.unwrap()));
+                }
+                VehicleSubCommand::Update => { println!("Update vehicle"); }
             }
         }
-        Err(()) => { println!("Invalid subcommand \"{}\"", sub_command)}
     }
-    
-    
-
-    // let data = r#"
-    //     {
-    //         "name": "Toyota Sienna",
-    //         "total_mileage": 1000,
-    //         "maintenance_items": {
-    //             "Oil Change": {
-    //                 "months_interval": 50,
-    //                 "miles_interval": 3,
-    //                 "name": "Oil Change",
-    //                 "description": "Synthetic 5w20",
-    //                 "last_performed_maintenance": {
-    //                     "miles_performed": 50,
-    //                     "date_performed": 0
-    //                 }
-    //             },
-    //             "Transmission Fluid Change": {
-    //                 "months_interval": 100,
-    //                 "miles_interval": 140,
-    //                 "name": "Transmission Fluid Change",
-    //                 "description": "Da Blue Stuff!",
-    //                 "last_performed_maintenance": {
-    //                     "miles_performed": 22,
-    //                     "date_performed": 0
-    //                 }
-    //             },
-    //             "Change all the bolts": {
-    //                 "months_interval": 900,
-    //                 "miles_interval": 140,
-    //                 "name": "Change all the bolts",
-    //                 "description": "Except spoiler bolts",
-    //                 "last_performed_maintenance": {
-    //                     "miles_performed": 999,
-    //                     "date_performed": 0
-    //                 }
-    //             }
-    //         }
-    //     }"#;
-
-    // let mut sienna = serde_json::from_str(data);
 
     // let mut sienna = Car {
     //     name: String::from("Toyota Sienna"),
@@ -97,6 +95,50 @@ fn main() {
 
     // dbg!(&sienna);
     // determine_needed_maintenance(sienna.unwrap())
+}
+
+fn load_cars() -> HashMap<String, Car> {
+    let data = r#"
+    {
+        "sienna": {
+            "name": "Toyota Sienna",
+            "total_mileage": 1000,
+            "maintenance_items": {
+                "Oil Change": {
+                    "months_interval": 50,
+                    "miles_interval": 3,
+                    "name": "Oil Change",
+                    "description": "Synthetic 5w20",
+                    "last_performed_maintenance": {
+                        "miles_performed": 50,
+                        "date_performed": 0
+                    }
+                },
+                "Transmission Fluid Change": {
+                    "months_interval": 100,
+                    "miles_interval": 140,
+                    "name": "Transmission Fluid Change",
+                    "description": "Da Blue Stuff!",
+                    "last_performed_maintenance": {
+                        "miles_performed": 22,
+                        "date_performed": 0
+                    }
+                },
+                "Change all the bolts": {
+                    "months_interval": 900,
+                    "miles_interval": 140,
+                    "name": "Change all the bolts",
+                    "description": "Except spoiler bolts",
+                    "last_performed_maintenance": {
+                        "miles_performed": 999,
+                        "date_performed": 0
+                    }
+                }
+            }
+        }
+    }"#;
+
+    serde_json::from_str(data).unwrap()
 }
 
 enum SubCommand {
